991f811b3620f4b39a4cdec21f40f4d369083beb
ArthurSpillere <69689479+ArthurSpillere@users.noreply.github.com>
2020-08-17 15:49:44 -0300

    (Exercícios da aula04)

---
+++ Exercícios Resolvidos/exercicio01.py
@@ -0,0 +1,15 @@
+
+# Exercicio 1
+# 
+# Imprima seu nome completo e idade cada um em uma nova linha 
+
+nomecompleto=str(input('Insira seu nome completo: '))
+idade=int(input('Insira sua idade: '))
+
+print('Nome:',nomecompleto,'\nIdade:',idade)
+
+#sem input abaixo
+
+print('Nome: Arthur Cordeiro\nIdade:27')
+
+
