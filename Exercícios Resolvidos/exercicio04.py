0cfd1f67316968d199623f83c3ae9b0724c5b70f
ArthurSpillere <69689479+ArthurSpillere@users.noreply.github.com>
2020-08-19 11:26:13 -0300

    Delete exercicio04.py

--- Exercícios Resolvidos/exercicio04.py
+++
@@ -1,8 +0,0 @@
-# Exercicio 4
-# 
-# Imprima na tela uma mensagem de boas vindas
-
-print('Bem Vindo ao menu de Python totalmente sem função até o momento. A não ser aprender print!\nEm breve teremos um software de ponta!\nAguarde!!!')
-
-
-
