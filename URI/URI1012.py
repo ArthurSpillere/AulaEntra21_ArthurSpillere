30121b6ef9e9eef200bede9053f6e8b7f4eb49c9
ArthurSpillere <69689479+ArthurSpillere@users.noreply.github.com>
2020-09-22 19:14:04 -0300

    Exercício URI - 1012
    Resolução do Exercício 1012 da plataforma URI

---
+++ URI/URI1012.py
@@ -0,0 +1,7 @@
+A1, B1, C1 = input().split()
+A, B, C = float(A1), float(B1), float(C1)
+print(f"TRIANGULO: {A*C/2:.3f}")
+print(f"CIRCULO: {3.14159*C**2:.3f}")
+print(f"TRAPEZIO: {((A+B)*C)/2:.3f}")
+print(f"QUADRADO: {B*B:.3f}")
+print(f"RETANGULO: {A*B:.3f}")
